7c4e118c565ad04413aec7e02a838839ad1d73ea
Guilherme Rojas <guilhermehrojas@gmail.com>
2015-08-30 22:07:52 -0300

    Reading command line

--- custom-bash/main.c
+++ custom-bash/main.c
@@ -1,11 +1,21 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
   char path[64];
+  int cmdlinesize = 64;
+  char *cmdline;
+
   getcwd(path, 64);
-  printf("\n[%s] ", path);
+  cmdline = malloc(64);
+  while(1) {
+    printf("\n[%s] ", path);
+    getline(&cmdline, (void *) &cmdlinesize, stdin);
+    printf("%s", cmdline);
+  }
+
   printf("\n");
   return 0;
 }
