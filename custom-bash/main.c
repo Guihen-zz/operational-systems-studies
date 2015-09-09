3f4b396819eccdf0e2462759ceac6b5376fa4708
Guilherme Rojas <guilhermehrojas@gmail.com>
2015-09-08 23:18:05 -0300

    Fixed execve

--- custom-bash/main.c
+++ custom-bash/main.c
@@ -1,15 +1,18 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 int cd_command(char *command_line) {
   return (command_line[0] == 'c' && command_line[1] == 'd' && command_line[2] == ' ');
 }
 
+char *cmd[] = { "/bin/ls", "-1", NULL };
 int main(int argc, char *argv[])
 {
   char path[64];
   char *command_line;
+  char *command_tokens[6];
   int comand_line_size = 64;
   int readed_chars, child_pid, command_response;
 
@@ -27,8 +30,14 @@ int main(int argc, char *argv[])
     }
     else {
       if ((child_pid = fork()) == 0) {
-        execve(command_line, argv, 0);
-        return 0;
+        for(int i = 0; i < 6; i++) {
+          command_tokens[i] = strsep(&command_line, " ");
+        }
+
+        if (execve(command_tokens[0], command_tokens, NULL)) {
+          printf("Invalid command\n");
+          return 0;
+        }
       }
       else {
         wait(&child_pid);
