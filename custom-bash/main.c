f6327f08b0079467696415daff4ae9709462eed8
Guilherme Rojas <guilhermehrojas@gmail.com>
2015-09-08 22:32:48 -0300

    child process dies after executes command

--- custom-bash/main.c
+++ custom-bash/main.c
@@ -5,24 +5,25 @@
 int main(int argc, char *argv[])
 {
   char path[64];
-  char *cmdline;
-  int cmdlinesize = 64;
-  int readedchars, childpid;
+  char *command_line;
+  int comand_line_size = 64;
+  int readed_chars, child_pid;
 
-  getcwd(path, 64);
-  cmdline = malloc(64);
+  command_line = malloc(64);
   printf("\n");
   while(1) {
+    getcwd(path, 64);
     printf("[%s] ", path);
 
-    readedchars = getline(&cmdline, (void *) &cmdlinesize, stdin);
-    cmdline[readedchars-1] = '\0';
+    readed_chars = getline(&command_line, (void *) &comand_line_size, stdin);
+    command_line[readed_chars-1] = '\0';
 
-    if ((childpid = fork()) == 0) {
-      execve(cmdline, argv, 0);
+    if ((child_pid = fork()) == 0) {
+      execve(command_line, argv, 0);
+      return 0;
     }
     else {
-      wait(&childpid);
+      wait(&child_pid);
     }
   }
 
