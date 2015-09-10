423e7f23d1534ff4562eed9e636f566c6ef5b63f
Guilherme Rojas <guilhermehrojas@gmail.com>
2015-09-10 20:17:15 -0300

    scheduler reads entire tracer file

--- custom-bash/scheduler.c
+++ custom-bash/scheduler.c
@@ -5,15 +5,18 @@ int main(int argc, char *argv[])
 {
   char *tracer_file_name;
   char process_name[16];
-  int t0, dt, deadline, priority;
+  int t0, dt, deadline, priority, status;
   void *tracer_content;
   FILE *tracer_file;
 
   tracer_file_name = argv[1];
   tracer_file = fopen(tracer_file_name, "r");
-  if (tracer_file)
+  while (tracer_file)
   {
-    fscanf(tracer_file, "%d %s %d %d %d", &t0, process_name, &dt, &deadline, &priority);
+    status = fscanf(tracer_file, "%d %s %d %d %d",
+      &t0, process_name, &dt, &deadline, &priority);
+    if (status == EOF) break;
+
     printf("%d %s %d %d %d\n", t0, process_name, dt, deadline, priority);
   }
   fclose(tracer_file);
