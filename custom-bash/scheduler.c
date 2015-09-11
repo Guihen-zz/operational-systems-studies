0233398defee9e0f5041fcb07184b133fdf0f2aa
Guilherme Rojas <guilhermehrojas@gmail.com>
2015-09-10 22:14:35 -0300

    Object to store process

--- custom-bash/scheduler.c
+++ custom-bash/scheduler.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct process_definition{
+  int t0;
+  int dt;
+  int deadline;
+  int priority;
+  char *pname;
+} * ProcessDefinition;
+
 int main(int argc, char *argv[])
 {
   char *tracer_file_name;
   char process_name[16];
   int t0, dt, deadline, priority, status;
   FILE *tracer_file;
+  ProcessDefinition PDCollection[64];
+  int PDcounter = 0;
 
   tracer_file_name = argv[1];
   tracer_file = fopen(tracer_file_name, "r");
@@ -16,8 +26,17 @@ int main(int argc, char *argv[])
       &t0, process_name, &dt, &deadline, &priority);
     if (status == EOF) break;
 
-    printf("%d %s %d %d %d\n", t0, process_name, dt, deadline, priority);
+    PDCollection[PDcounter] = malloc(sizeof(PDCollection));
+    PDCollection[PDcounter]->t0 = t0;
+    PDCollection[PDcounter]->pname = process_name;
+    PDCollection[PDcounter]->dt = dt;
+    PDCollection[PDcounter]->deadline = deadline;
+    PDCollection[PDcounter]->priority = priority;
+    PDcounter++;
   }
+
+  printf("%d %s %d %d %d\n", PDCollection[0]->t0, PDCollection[0]->pname, PDCollection[0]->dt, PDCollection[0]->deadline, PDCollection[0]->priority);
+  printf("%d %s %d %d %d\n", PDCollection[1]->t0, PDCollection[1]->pname, PDCollection[1]->dt, PDCollection[1]->deadline, PDCollection[1]->priority);
   fclose(tracer_file);
 
   return 0;
