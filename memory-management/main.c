bead0339da0d15bffa658fc1ce4fa65ba809d750
Guilherme Rojas <guilhermehrojas@gmail.com>
2015-10-06 12:07:57 -0300

    creating memory files

--- memory-management/main.c
+++ memory-management/main.c
@@ -5,16 +5,29 @@ int main( int argc, char *argv[]) {
   char *tracefile_name;
   FILE *tracefile;
   int memory_size, virtual_memory_size;
+  FILE *memory_file, *virtual_memory_file;
+  int i;
+  short c = -1;
 
   tracefile_name = argv[1];
   tracefile = fopen( tracefile_name, "r");
   if( tracefile) {
-    fscanf(tracefile, "%d %d", &memory_size, &virtual_memory_size);
+    fscanf( tracefile, "%d %d", &memory_size, &virtual_memory_size);
   } else {
     return 1;
   }
 
-  printf("%d %d", memory_size, virtual_memory_size);
+  memory_file = fopen( "/tmp/ep2.mem", "wb");
+  for( i = 0; i < memory_size; i++) {
+    fwrite( &c, 1, 1, memory_file);
+  }
+  fclose(memory_file);
+
+  virtual_memory_file = fopen("/tmp/ep2.vir", "wb");
+  for( i = 0; i < virtual_memory_size; i++) {
+    fwrite( &c, 1, 1, virtual_memory_file);
+  }
+  fclose(virtual_memory_file);
 
   return 0;
 }
