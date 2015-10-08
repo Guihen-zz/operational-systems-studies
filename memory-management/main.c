810f0e97de3d15fae5573990017d255b8cc6c88a
Guilherme Rojas <guilhermehrojas@gmail.com>
2015-10-08 17:37:46 -0300

    reading entire line from tracefile

--- memory-management/main.c
+++ memory-management/main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #define EMPTY_SPACE -1
 #define PROCESS_NAME_MAX_LENGHT 16
@@ -13,7 +14,7 @@ struct access_request {
 typedef struct process_definition {
   float t0;
   float tf;
-  char name[PROCESS_NAME_MAX_LENGHT];
+  char *name;
   int b;
   struct access_request access_requests[MAX_ACCESS_REQUEST];
   int access_requests_counter;
@@ -36,13 +37,17 @@ int main( int argc, char *argv[]) {
   int memory_size, virtual_memory_size;
   FILE *memory_file, *virtual_memory_file;
   Experiment experiment;
-  int i;
+  int i, j;
   struct access_request access_request;
+  size_t len = 0;
+  ssize_t read;
+  char *line = malloc(128);
 
   tracefile_name = argv[1];
   tracefile = fopen( tracefile_name, "r");
   if( tracefile) {
-    fscanf( tracefile, "%d %d", &memory_size, &virtual_memory_size);
+    read = getline(&line, &len, tracefile);
+    sscanf( line, "%d %d", &memory_size, &virtual_memory_size);
   } else {
     return 1;
   }
@@ -51,14 +56,17 @@ int main( int argc, char *argv[]) {
   virtual_memory_file = generate_virtual_memory_file(virtual_memory_size);
   experiment = generate_experiment(tracefile);
 
-  printf("%f %s %f %d\n",
-    experiment->trials[0]->t0,
-    experiment->trials[0]->name,
-    experiment->trials[0]->tf,
-    experiment->trials[0]->b);
+  for(j = 0; j < experiment->trials_counter; j++) {
+    printf("%f %s %f %d\n",
+      experiment->trials[j]->t0,
+      experiment->trials[j]->name,
+      experiment->trials[j]->tf,
+      experiment->trials[j]->b);
 
-  for(i = 0; experiment->trials[0]->access_requests_counter > i; i++) {
-    printf("%f %d\n", experiment->trials[0]->access_requests[i].t, experiment->trials[0]->access_requests[i].p);
+    for(i = 0; experiment->trials[j]->access_requests_counter > i; i++) {
+      printf("%f %d ", experiment->trials[j]->access_requests[i].t, experiment->trials[j]->access_requests[i].p);
+    }
+    printf("\n");
   }
 
   fclose(memory_file);
@@ -86,29 +94,54 @@ FILE * generate_virtual_memory_file(int size) {
 Experiment generate_experiment(FILE *tracefile) {
   Experiment experiment = malloc(sizeof(*experiment));
   ProcessDefinition process_definition;
-  int access_requests_counter, pi;
+  int access_requests_counter, trials_counter, pi;
   float ti;
+  int end_of_file = 0;
+  char *line = malloc(128);
+  size_t len = 0;
+  ssize_t read;
+  char *token;
+  char  *str;
+  float f;
+  int i;
 
-  experiment->trials = malloc(sizeof(*experiment->trials)* MAX_PROCESS_SIZE);
-  process_definition = malloc(sizeof(*process_definition));
-  fscanf(tracefile, "%f %s %f %d",
-    &process_definition->t0,
-    process_definition->name,
-    &process_definition->tf,
-    &process_definition->b);
+  experiment->trials = malloc(sizeof(ProcessDefinition)* MAX_PROCESS_SIZE);
+  trials_counter = 0;
+  while( (read = getline(&line, &len, tracefile)) > 0) {
+    process_definition = malloc( sizeof( *process_definition));
 
-  access_requests_counter = 0;
-  while(1) {
-    if( fscanf(tracefile, "%f", &ti) == EOF || ti == '\n') break;
+    token = strsep(&line, " ");
+    sscanf(token, "%f", &f);
+    process_definition->t0 = f;
 
-    fscanf(tracefile, "%d", &pi);
-    process_definition->access_requests[access_requests_counter].t = ti;
-    process_definition->access_requests[access_requests_counter].p = pi;
-    access_requests_counter++;
+    process_definition->name = strsep(&line, " ");
+
+    token = strsep(&line, " ");
+    sscanf(token, "%f", &f);
+    process_definition->tf = f;
+
+    token = strsep(&line, " ");
+    sscanf(token, "%d", &i);
+    process_definition->b = i;
+
+    access_requests_counter = 0;
+    while(line != NULL) {
+      token = strsep(&line, " ");
+      sscanf(token, "%f", &ti);
+
+      token = strsep(&line, " ");
+      sscanf(token, "%d", &pi);
+
+      process_definition->access_requests[access_requests_counter].t = ti;
+      process_definition->access_requests[access_requests_counter].p = pi;
+      access_requests_counter++;
+    }
+
+    process_definition->access_requests_counter = access_requests_counter;
+    experiment->trials[trials_counter] = process_definition;
+    trials_counter++;
   }
 
-  process_definition->access_requests_counter = access_requests_counter;
-  experiment->trials[0] = process_definition;
-  experiment->trials_counter = 0;
+  experiment->trials_counter = trials_counter;
   return experiment;
 }
